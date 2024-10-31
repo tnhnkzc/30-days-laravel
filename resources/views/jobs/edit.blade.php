5f01cca5b2fccaed6c78b04035bd00258183db17
Tunahan Kuzucu <tunahankuzucuu@gmail.com>
2024-10-31 22:49:49 +0100

    day-18-edit-update-delete

--- resources/views/jobs/edit.blade.php
+++ resources/views/jobs/edit.blade.php
@@ -1,15 +1,13 @@
 <x-layout>
     <x-slot:heading>
-        Create a Job
+        Edit a Job: {{ $job->title }}
     </x-slot:heading>
 
-    <form method="POST" action="/jobs">
+    <form method="POST" action="/jobs/{{ $job->id }}">
         @csrf
+        @method('PATCH')
         <div class="space-y-12">
             <div class="border-b border-gray-900/10 pb-12">
-                <h2 class="text-base/7 font-semibold text-gray-900">Create a Job</h2>
-                <p class="mt-1 text-sm/6 text-gray-600">We just need a handful of details from you.</p>
-
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                     <div class="sm:col-span-4">
                         <label for="title" class="block text-sm/6 font-medium text-gray-900">Title</label>
@@ -18,7 +16,7 @@
                                 class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                                 <input type="text" name="title" id="title" autocomplete="title"
                                     class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm/6"
-                                    placeholder="Software Engineer" required>
+                                    placeholder="Software Engineer" value="{{ $job->title }}" required>
                             </div>
                             @error('title')
                                 <p class="text-sm text-red-500 font-semibold mt-1">{{ $message }}</p>
@@ -31,7 +29,7 @@
                         <div class="mt-2">
                             <div
                                 class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                <input type="text" name="salary" id="salary"
+                                <input type="text" name="salary" id="salary" value="{{ $job->salary }}"
                                     class="block flex-1 border-0
                                     bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0
                                     sm:text-sm/6"
@@ -42,12 +40,24 @@
                             @enderror
                         </div>
                     </div>
+                </div>
 
-                    <div class="mt-6 flex items-center justify-end gap-x-6">
-                        <x-button href="/jobs">Cancel</x-button>
-                        <button type="submit"
-                            class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+                <div class="mt-6 flex items-center justify-between gap-x-6">
+                    <div class="flex items-center">
+                        <button form="delete-form"
+                            class="text-sm font-semibold leading-6 px-3 py-2 rounded text-gray-900 bg-red-500">Delete</button>
                     </div>
+                    <div class="flex items-center gap-x-6">
+                        <a href="/jobs/{{ $job->id }}"
+                            class="text-sm font-semibold leading-6 text-gray-900">Cancel</a>
+                        <button type="submit"
+                            class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Update</button>
+                    </div>
+                </div>
+    </form>
+    <form method="POST" action="/jobs/{{ $job->id }}" id="delete-form" class="hidden">
+        @csrf
+        @method('DELETE')
     </form>
 
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ Route::get('/', function () {
     return view('home');
 });
 
+// Index
 Route::get('/jobs', function () {
     $jobs = Job::with('employer')->latest()->simplePaginate(3);
 
@@ -15,10 +16,13 @@ Route::get('/jobs', function () {
     ]);
 });
 
+// Create
 Route::get('/jobs/create', function () {
     return view('jobs.create');
 });
 
+
+// Store
 Route::post('/jobs', function () {
     // validation
     request()->validate([
@@ -34,12 +38,57 @@ Route::post('/jobs', function () {
     return redirect('/jobs');
 });
 
+// Show
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
 
     return view('jobs.show', ['job' => $job]);
 });
 
+// Edit
+Route::get('/jobs/{id}/edit', function ($id) {
+    $job = Job::find($id);
+
+    return view('jobs.edit', ['job' => $job]);
+});
+
+// Update
+Route::patch('/jobs/{id}', function ($id) {
+    // validation
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required']
+    ]);
+
+    // Authorize (On hold)
+
+    // Update the job
+    $job = Job::findOrFail($id);
+    $job->title = request('title');
+    $job->salary = request('salary');
+    $job->save();
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary')
+    ]);
+
+    // redirect (job specific)
+    return redirect('/jobs/' . $job->id);
+});
+
+// Destroy
+Route::delete('/jobs/{id}', function ($id) {
+
+    // Authorize (On hold)
+
+
+    // Delete 
+    Job::findOrFail($id)->delete();
+
+    // redirect 
+    return redirect('/jobs');
+});
 
 Route::get('/contact', function () {
     return view('contact');
